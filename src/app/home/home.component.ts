53472acac651d9b42a55e24d79b758bdb19dbdc7
wilofitz <gfitzpatrick@sistemasw.com.ar>
2019-06-13 07:46:28 -0300

    chamge summary data load to show incomes_nc

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { ISummary, ISummaryDay } from '../models';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   selectedTab: number = 0;
-  constructor() { }
+  summary: ISummary;
+  days: ISummaryDay[] = []
+  slideIndex: number = 0;
+  constructor(private ds: DataService) { }
 
   ngOnInit() {
+    this.getData();
   }
 
+  getData() {
+    this.summary = null;
+    this.ds.getSummary().subscribe((data) => {
+        //inicializo los dias
+        let _today = new Date();
+        let _isoToday = _today.toISOString();
+        for (let i=0; i<=6;i++) 
+        {
+            let _day = new Date(data.inicio);
+            let _isoDay = _day.toISOString();
+
+            try {
+                _day.setDate(  (_day.getDate() + i) );
+                _isoDay = _day.toISOString();
+            } catch {
+                console.warn("Error seting _date for further retrieve _expense for slider")
+            }
+
+            try {
+                if (_isoDay.substring(0,10) === _isoToday.substring(0,10)) {
+                    this.slideIndex = (i === 6) ? 5 : i;
+                }
+            } catch {
+                console.warn("Error seting current index of slider")
+            }
+
+            let _expense = 0;
+            try {
+
+                let day;
+                for (day of data.SummaryDays) {
+                    if (day.date.toString().substring(0,10) === _isoDay.substring(0,10)){
+                        _expense = _expense + day.expense;
+                        break;
+                    }
+                }
+            } catch {
+                console.warn("Error geting _expense from SummaryDays for slider")
+
+            }
+
+            this.days.push( {date: _day , expense: (_expense || 0) })
+        }      
+
+        this.summary = data;
+        console.log( "summary is ready")
+    });
+}
 }
--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, AfterViewInit, ViewChild, Renderer2, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterViewInit, ViewChild, Renderer2, ChangeDetectorRef, Input } from '@angular/core';
 import { DataService } from '../data.service';
 import { ISummary, ISummaryDay } from '../models';
 import { lory, Lory } from 'lory.js';
@@ -10,24 +10,25 @@ import { isMobile } from '../utils';
     styleUrls: ['./summary.component.scss']
 })
 export class SummaryComponent implements OnInit {
+    @Input("summary") summary: ISummary;
+    @Input("summary-days") days: ISummaryDay[];
+    @Input("slide-index") slideIndex: number = 0;
 
     private slider: ElementRef;
     private prev: ElementRef;
     private next: ElementRef;
-    private slideIndex: number = 0;
+
     @ViewChild("slider", { read: ElementRef, static: false }) set sliderElRef(slider: ElementRef) {
         this.slider = slider;
-        //setup the slider
+        if (!this.slider) return;
         setTimeout( () => {
             this.lorySlider = lory( this.slider.nativeElement, { infinite: false, slidesToScroll: 2, initialIndex: this.slideIndex });
-
+            console.log( "lory is initialized !")
             if (isMobile()) {
                 this.r2.setStyle(  this.prev.nativeElement, "display", "none");
                 this.r2.setStyle(  this.next.nativeElement, "display", "none");
             }
         },100);
-
-
     }
 
     @ViewChild("prev", { read: ElementRef, static: false }) set prevElRef(prev: ElementRef) {
@@ -37,63 +38,63 @@ export class SummaryComponent implements OnInit {
       this.next = next;
     }
 
-    summary: ISummary;
     lorySlider: any;
-    dias: ISummaryDay[] = []
+    // summary: ISummary;
+    // dias: ISummaryDay[] = []
 
-    constructor(private ds: DataService,
+    constructor(/*private ds: DataService,*/
                 private r2: Renderer2) { }
 
     ngOnInit() {
-        this.getData();
+        //this.getData();
     }
 
-    getData() {
-        this.ds.getSummary().subscribe((data) => {
-            //inicializo los dias
-            let _today = new Date();
-            let _isoToday = _today.toISOString();
-            for (let i=0; i<=6;i++) 
-            {
-                let _day = new Date(data.inicio);
-                let _isoDay = _day.toISOString();
+    // getData() {
+    //     this.ds.getSummary().subscribe((data) => {
+    //         //inicializo los dias
+    //         let _today = new Date();
+    //         let _isoToday = _today.toISOString();
+    //         for (let i=0; i<=6;i++) 
+    //         {
+    //             let _day = new Date(data.inicio);
+    //             let _isoDay = _day.toISOString();
 
-                try {
-                    _day.setDate(  (_day.getDate() + i) );
-                    _isoDay = _day.toISOString();
-                } catch {
-                    console.warn("Error seting _date for further retrieve _expense for slider")
-                }
+    //             try {
+    //                 _day.setDate(  (_day.getDate() + i) );
+    //                 _isoDay = _day.toISOString();
+    //             } catch {
+    //                 console.warn("Error seting _date for further retrieve _expense for slider")
+    //             }
 
-                try {
-                    if (_isoDay.substring(0,10) === _isoToday.substring(0,10)) {
-                        this.slideIndex = (i === 6) ? 5 : i;
-                    }
-                } catch {
-                    console.warn("Error seting current index of slider")
-                }
+    //             try {
+    //                 if (_isoDay.substring(0,10) === _isoToday.substring(0,10)) {
+    //                     this.slideIndex = (i === 6) ? 5 : i;
+    //                 }
+    //             } catch {
+    //                 console.warn("Error seting current index of slider")
+    //             }
 
-                let _expense = 0;
-                try {
+    //             let _expense = 0;
+    //             try {
 
-                    let day;
-                    for (day of data.SummaryDays) {
-                        if (day.date.toString().substring(0,10) === _isoDay.substring(0,10)){
-                            _expense = _expense + day.expense;
-                            break;
-                        }
-                    }
-                } catch {
-                    console.warn("Error geting _expense from SummaryDays for slider")
+    //                 let day;
+    //                 for (day of data.SummaryDays) {
+    //                     if (day.date.toString().substring(0,10) === _isoDay.substring(0,10)){
+    //                         _expense = _expense + day.expense;
+    //                         break;
+    //                     }
+    //                 }
+    //             } catch {
+    //                 console.warn("Error geting _expense from SummaryDays for slider")
 
-                }
+    //             }
 
-                this.dias.push( {date: _day , expense: (_expense || 0) })
-            }      
+    //             this.dias.push( {date: _day , expense: (_expense || 0) })
+    //         }      
 
-            this.summary = data;
+    //         this.summary = data;
             
-        });
-    }
+    //     });
+    // }
 
 }
